6fb094633746ad340eeb81fd9c5bba1fa6c0d0ef
hiep <hiep.dao@leximit.com>
2023-11-02 19:04:59 +0700

    sticky menu for PAGE MENU

--- wp-content/themes/dom/js/script.js
+++ wp-content/themes/dom/js/script.js
@@ -126,17 +126,109 @@ $(document).ready(function () {
   $('.defaultSlick').slick(settings);
 
   // Menu Page
-  $('.tabControl ._tab').click(function () {
-    let current = $(this);
-    let targetTab = current.attr('data-id');
-    // Toggle Effect
-    $('.tabControl ._tab').removeClass('activated');
-    current.addClass('activated');
-    $('.tabContent ._content').removeClass('activated');
-    $('#' + targetTab).addClass('activated');
+  let pageMENU = {
+    _currentWidth: $(window).width(),
+    _header: $('header.site-header'),
+    _tabControl: $('.menu_section .tabControl'),
+    _tabContent: $('.menu_section .tabContent'),
+    _tabContentHeight: $(window).height(),
+    _listenTabContentScrollIsEnable: false,
+    _lastScroll: 0,
 
-    // Update the size of slick slider
-    $('.defaultSlick').slick('setPosition');
-  })
+    init: function () {
+      this.listenTabControlClick();
+      this.initStickyMenu();
+    },
+
+    listenTabControlClick: function () {
+      console.log('=== listenTabControlClick');
+      $('.tabControl ._tab').click(function () {
+        let current = $(this);
+        let targetTab = current.attr('data-id');
+        // Toggle Effect
+        $('.tabControl ._tab').removeClass('activated');
+        current.addClass('activated');
+        $('.tabContent ._content').removeClass('activated');
+        $('#' + targetTab).addClass('activated');
+        // Update the size of slick slider
+        $('.defaultSlick').slick('setPosition');
+      })
+    },
+
+    initStickyMenu: function () {
+      this.doResize();
+      this.listenResize();
+    },
+
+    listenResize: function () {
+      let _this = this;
+      $(window).resize(function () {
+        _this.doResize();
+      });
+    },
+
+    doResize: function () {
+      let _this = this;
+      _this._currentWidth = $(window).width();
+      if (_this._currentWidth < 1200) {
+        _this.reset();
+        return false;
+      }
+
+      let curBody = $("html, body");
+      curBody.stop().animate({scrollTop: 0}, 200);
+      _this.offBodyScroll();
+
+      let menuHeight = _this._header.outerHeight();
+      let tabControlHeight = _this._tabControl.outerHeight();
+      let tabContentHeight = $(window).height() - menuHeight - tabControlHeight;
+      _this._tabContent.css('height', tabContentHeight + 'px');
+
+      // if (!_this._listenTabContentScrollIsEnable) {
+      //   _this.listenTabContentScroll();
+      // }
+
+      return tabContentHeight;
+    },
+
+    listenTabContentScroll: function () {
+      let _this = this;
+      _this._tabContent.on('scroll', function () {
+        let curScrollTop = _this._tabContent.scrollTop();
+        _this._lastScroll = _this._tabContent.scrollTop();
+
+        let curTabContentHeight = _this._tabContent.height();
+        let scrollHeight = _this._tabContent.prop('scrollHeight');
+
+        // if ((curScrollTop + curTabContentHeight) > (scrollHeight - 10)) {
+        //   console.log("end");
+        //   _this.onBodyScroll();
+        //   _this.listenBodyScroll();
+        //   _this._tabContent.css('overflow-y', 'hidden');
+        // } else {
+        //   $('body.page-menu').animate({scrollTop: 0}, 100);
+        //   _this.offBodyScroll();
+        //   _this._tabContent.css('overflow-y', 'auto');
+        // }
+      });
+    },
+
+    onBodyScroll: function () {
+      $('body.page-menu').css('overflow-y', 'auto');
+    },
+
+    offBodyScroll: function () {
+      $('body.page-menu').css('overflow-y', 'hidden');
+    },
+
+    reset: function () {
+      this._tabContent.attr('style', '');
+      $('body.page-menu').attr('style', '');
+    }
+  }
+
+  if (body.is('.page-menu, .page')) {
+    pageMENU.init();
+  }
 
 });
